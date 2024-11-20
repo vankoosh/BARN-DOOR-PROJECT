04a306d8b809ae40b030c5a05c979258e9da570b
vankoosh <vankoosh@gmail.com>
2024-11-20 12:15:53 +0100

    new form fields

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const logo = document.querySelector("#logo");
 const offer = document.querySelector("#offer");
+const form = document.querySelector(".contact-form__inputs");
 
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
@@ -25,3 +26,42 @@ function logoDown(scroll) {
 }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '787180756425947');
 fbq('track', 'PageView');
+
+const uhlikoveSystemyArray = ["Vyberte systém, o ktorý máte záujem",
+  "Systém RLD 08", "Systém RLD 08 Biely", "Systém RLD 09", "Systém RLD 10", "Systém RLD 11", "Systém RLD 12", "Systém RLD 13", "Systém RLD 14", "Systém RLD 16", "Systém RLD 17", "Systém RLD 18", "Systém RLD 19", "Systém RLD 20", "Systém RLD 21", "Systém RLD 22", "Systém RLD 23", "Systém RLD 24", "Systém RLD 25", "Systém RLD 26", "Systém RLD 27", "Systém RLD 28", "Systém RLD 29", "Rebrik R1"]
+
+const nerezSystemyArray = ["S8", "S9", "S11", "S12", "S13", "S15", "S17", "S18"]
+
+const patinaSystemyArray = ["Patina HRDZA", "Patina ZELENA", "Patina BRONZ", "Patina BRONZ P5"]
+
+const skloSystemyArray = ["Systém G1", "Systém G2", "Systém G3"]
+
+const miniSystemyArray = ["Mini 1", "Mini 2", "Mini 3", "Mini 4", "Mini 5", "Mini 6"]
+
+const bypassSystemyArray = ["Systém BYPASS"]
+
+const mergedArray = [...uhlikoveSystemyArray, ...nerezSystemyArray, ...patinaSystemyArray, ...skloSystemyArray, ...miniSystemyArray, ...bypassSystemyArray]
+
+if (form) {
+  const selectElement = document.createElement('select');
+  selectElement.name = 'systemy';
+  selectElement.id = 'systemy';
+
+  mergedArray.forEach((element, index) => {
+    if (index == 0) {
+      const optionElement = document.createElement('option');
+      optionElement.value = '';
+      optionElement.innerText = `${element}`;
+      optionElement.disabled = true;
+      optionElement.selected = true;
+      selectElement.appendChild(optionElement);
+    } else {
+      const optionElement = document.createElement('option');
+      optionElement.value = element;
+      optionElement.innerText = `${element}`;
+      selectElement.appendChild(optionElement);
+    }
+  });
+
+  form.appendChild(selectElement);
+}
