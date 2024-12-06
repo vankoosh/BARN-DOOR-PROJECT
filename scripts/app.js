d0d14f170667e791a1e54f9e8d6223ebc69a4793
vankoosh <vankoosh@gmail.com>
2024-12-06 15:42:12 +0100

    done

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const logo = document.querySelector("#logo");
 const offer = document.querySelector("#offer");
 const form = document.querySelector(".contact-form__inputs");
+const productCards = document.querySelectorAll(".card-sys");
 
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
@@ -27,41 +28,93 @@ function logoDown(scroll) {
 fbq('init', '787180756425947');
 fbq('track', 'PageView');
 
-const uhlikoveSystemyArray = ["Vyberte systém, o ktorý máte záujem",
+const uhlikoveSystemy = [
   "Systém RLD 08", "Systém RLD 08 Biely", "Systém RLD 09", "Systém RLD 10", "Systém RLD 11", "Systém RLD 12", "Systém RLD 13", "Systém RLD 14", "Systém RLD 16", "Systém RLD 17", "Systém RLD 18", "Systém RLD 19", "Systém RLD 20", "Systém RLD 21", "Systém RLD 22", "Systém RLD 23", "Systém RLD 24", "Systém RLD 25", "Systém RLD 26", "Systém RLD 27", "Systém RLD 28", "Systém RLD 29", "Rebrik R1"]
 
-const nerezSystemyArray = ["Systém S8", "Systém S9", "Systém S11", "Systém S12", "Systém S13", "Systém S15", "Systém S17", "Systém S18"]
+const nerezSystemy = ["Systém S8", "Systém S9", "Systém S11", "Systém S12", "Systém S13", "Systém S15", "Systém S17", "Systém S18"]
 
-const patinaSystemyArray = ["Patina HRDZA", "Patina ZELENA", "Patina BRONZ", "Patina BRONZ P5"]
+const patinaSystemy = ["Patina HRDZA", "Patina ZELENA", "Patina BRONZ", "Patina BRONZ P5"]
 
-const skloSystemyArray = ["Systém G1", "Systém G2", "Systém G3"]
+const skloSystemy = ["Systém G1", "Systém G2", "Systém G3"]
 
-const miniSystemyArray = ["Mini 1", "Mini 2", "Mini 3", "Mini 4", "Mini 5", "Mini 6"]
+const miniSystemy = ["Mini 1", "Mini 2", "Mini 3", "Mini 4", "Mini 5", "Mini 6"]
 
-const bypassSystemyArray = ["Systém BYPASS"]
+const bypassSystemy = ["Systém BYPASS"]
 
-const mergedArray = [...uhlikoveSystemyArray, ...nerezSystemyArray, ...patinaSystemyArray, ...skloSystemyArray, ...miniSystemyArray, ...bypassSystemyArray]
+const mergedArray = [
+  "Vyberte systém, o ktorý máte záujem",
+  ...uhlikoveSystemy,
+  ...nerezSystemy,
+  ...patinaSystemy,
+  ...skloSystemy,
+  ...miniSystemy,
+  ...bypassSystemy
+]
 
 if (form) {
   const selectElement = document.createElement('select');
+  const fromQuery = window.location.href.indexOf('?') + 5;
+  const toQuery = window.location.href.indexOf('#');
+  const selectedSystem = decodeURIComponent(window.location.href.slice(fromQuery, toQuery));
+
   selectElement.name = 'systemy';
   selectElement.id = 'systemy';
 
   mergedArray.forEach((element, index) => {
-    if (index == 0) {
-      const optionElement = document.createElement('option');
-      optionElement.value = '';
-      optionElement.innerText = `${element}`;
-      optionElement.disabled = true;
-      optionElement.selected = true;
-      selectElement.appendChild(optionElement);
-    } else {
-      const optionElement = document.createElement('option');
-      optionElement.value = element;
-      optionElement.innerText = `${element}`;
-      selectElement.appendChild(optionElement);
-    }
+    const optionElement = document.createElement('option');
+    optionElement.value = element;
+    optionElement.innerText = `${element}`;
+    selectElement.appendChild(optionElement);
   });
 
+  if (selectedSystem) {
+    let selectedIndex;
+    const selectedChoice = Array.from(selectElement.children).forEach((e, index) => {
+      if (e.value == selectedSystem) {
+        selectedIndex = index
+      }
+    })
+    selectElement.selectedIndex = selectedIndex;
+  }
   form.appendChild(selectElement);
+}
+
+if (productCards) {
+  let system;
+  const sysId = document.querySelector('main').dataset.sysId
+  console.log(sysId);
+
+  switch (sysId) {
+    case 'uhlikoveSystemy':
+      system = uhlikoveSystemy;
+      console.log(system);
+      break;
+    case 'nerezSystemy':
+      system = nerezSystemy;
+      console.log(system);
+      break;
+    case 'patinaSystemy':
+      system = patinaSystemy;
+      console.log(system);
+      break;
+    case 'skloSystemy':
+      system = skloSystemy;
+      console.log(system);
+      break;
+    case 'bypassSystemy':
+      system = bypassSystemy;
+      console.log(system);
+      break;
+    default:
+      break;
+  }
+  productCards.forEach((card, index) => {
+    const textContainer = card.querySelector('.card-sys__text')
+    const bookButton = document.createElement('a');
+    bookButton.setAttribute('class', 'card-sys__book-button');
+    bookButton.setAttribute('href', 'http://127.0.0.1:5500/index.html?sys=' + system[index] + '#contact-form');
+    bookButton.setAttribute('data-sys-id', system[index]);
+    bookButton.innerText = 'Objednať'
+    textContainer.appendChild(bookButton);
+  })
 }
