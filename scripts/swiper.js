66b0f48b5e245603c24beabb2bfcb927fa99d70d
vankoosh <vankoosh@gmail.com>
2024-12-07 17:03:24 +0100

    swiper

---
+++ scripts/swiper.js
@@ -0,0 +1,14 @@
+// import Swiper from "swiper";
+
+// const swiperElement = document.querySelector(".swiper");
+
+// if (swiperElement) {
+//   console.log('swiper');
+
+//   const swiper = new Swiper('.swiper', {
+//     speed: 400,
+//     spaceBetween: 100,
+//   });
+// }
+
+// https://swiperjs.com/swiper-api
